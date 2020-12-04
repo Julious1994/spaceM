19887262be051a945f0b6d9f0bb73bdba9bf8ef0
Amit Patel <a.patel@axelor.com>
2020-12-04 21:15:57 +0530

    Create common style

---
+++ src/commonStyles.js
@@ -0,0 +1,28 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+	pageStyle: {
+		height: '100%',
+		backgroundColor: '#000F24',
+	},
+	largePageSize: {
+		paddingLeft: '12%',
+		paddingRight: '12%',
+	},
+	compactPageStyle: {
+		paddingLeft: '5%',
+		paddingRight: '5%',
+	},
+	backButton: {
+		position: 'absolute',
+		left: 10,
+		top: 10,
+		padding: 10,
+	},
+	backButtonIcon: {
+		width: 10,
+		height: 16,
+	},
+});
+
+export default styles;
