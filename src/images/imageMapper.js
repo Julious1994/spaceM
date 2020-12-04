afdb948331059437b29c956bc9aeddcd92ddee03
Amit Patel <a.patel@axelor.com>
2020-12-04 21:14:09 +0530

    Add images

---
+++ src/images/imageMapper.js
@@ -0,0 +1,107 @@
+export default {
+	moviePhoto: {
+		source: require('./moviePhoto.jpg'),
+	},
+	moviePhoto_2: {
+		source: require('./moviePhoto_2.jpg'),
+	},
+	moviePhoto2: {
+		source: require('./moviePhoto2.png'),
+	},
+	logoIcon: {
+		source: require('./logoIcon.png'),
+	},
+	searchIcon: {
+		source: require('./searchIcon.png'),
+	},
+	search: {
+		source: require('./search.png'),
+	},
+	verticalDot: {
+		source: require('./verticalDot.png'),
+	},
+	removeWatchListItem: {
+		source: require('./removeWatchListItem.png'),
+	},
+	info: {
+		source: require('./info.png'),
+	},
+	spaceMLogo: {
+		source: require('./spaceMLogo.png'),
+	},
+	plus: {
+		source: require('./plus.png'),
+	},
+	successPayment: {
+		source: require('./successPayment.png'),
+	},
+	failurePayment: {
+		source: require('./failurePayment.png'),
+	},
+	rightArrow: {
+		source: require('./rightArrow.png'),
+	},
+	leftArrow: {
+		source: require('./leftArrow.png'),
+	},
+	watchListMenu: {
+		source: require('./watchListMenu.png'),
+	},
+	user: {
+		source: require('./user.png'),
+	},
+	activehome: {
+		source: require('./activehome.png'),
+	},
+	home: {
+		source: require('./home.png'),
+	},
+	play: {
+		source: require('./play.png'),
+	},
+	roundPlay: {
+		source: require('./playButton.png'),
+	},
+	playButton: {
+		source: require('./playButtonRect.png'),
+	},
+	share: {
+		source: require('./share.png'),
+	},
+	favourite: {
+		source: require('./favourite.png'),
+	},
+	plusRound: {
+		source: require('./plusRound.png'),
+	},
+	crown: {
+		source: require('./crown.png'),
+	},
+	featureMoview: {
+		source: require('./featureMovie.png'),
+	},
+	bell: {
+		source: require('./bell.png'),
+	},
+	menu: {
+		source: require('./menu.png'),
+	},
+	landscapeMovie: {
+		source: require('./landscape.png'),
+	},
+	landscapeMovie_2: {
+		source: require('./landscape2.jpg'),
+	},
+	videoPlayer: {
+		source: require('./VideoPlayer.jpg'),
+	},
+	pencil: {
+		source: require('./pencil.png'),
+	},
+	forwardPlay: {
+		source: require('./forwardPlay.png'),
+	},
+	backwardPlay: {
+		source: require('./backwardPlay.png'),
+	},
+};
