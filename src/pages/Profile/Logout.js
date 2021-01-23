d4c180627c93909f1b839f218480337d78c2f696
Amit Patel <amitpatel23894@gmail.com>
2021-01-23 21:42:48 +0530

    Logout from google signin

--- src/pages/Profile/Logout.js
+++ src/pages/Profile/Logout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Alert} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {StackActions} from '@react-navigation/native';
+import { GoogleSignin } from '@react-native-community/google-signin';
 
 export function logout(navigation) {
 	Alert.alert('Logout', 'Do you want to logut', [
@@ -9,7 +10,13 @@ export function logout(navigation) {
 			text: 'YES',
 			onPress: async () => {
 				await AsyncStorage.removeItem('user');
-				navigation.dispatch(StackActions.replace('Login'));
+				navigation.dispatch(StackActions.replace('LoginMenu'));
+				try {
+					await GoogleSignin.revokeAccess();
+					await GoogleSignin.signOut();
+				  } catch (error) {
+					console.error(error);
+				  }
 			},
 		},
 		{text: 'NO'},
