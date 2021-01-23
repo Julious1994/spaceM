b2f8e2a5b5feb71fa2461f9b725134a3f4708af4
Amit Patel <amitpatel23894@gmail.com>
2021-01-23 21:49:30 +0530

    Update dependency

--- react-native.config.js
+++ react-native.config.js
@@ -5,5 +5,12 @@ module.exports = {
 				android: null, // disable Android platform, other platforms will still autolink if provided
 			},
 		},
+		'react-native-video': {
+            platforms: {
+                android: {
+                    sourceDir: '../node_modules/react-native-video/android-exoplayer',
+                },
+            },
+        }
 	},
 };
