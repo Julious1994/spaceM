a2ef808082b0fbe032735c5189558333f977f032
Amit Patel <amitpatel23894@gmail.com>
2021-01-23 21:47:21 +0530

    Add routes

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ import Transactions from './pages/Profile/Transactions';
 import Video from './pages/Video/Video';
 import VideoDetail from './pages/Video/VideoDetail';
 import PaymentForm from './pages/Payment/PaymentForm';
+import PaymentView from './pages/Payment/PaymentView';
 import SearchView from './pages/SearchMovie';
 import ProfileView from './pages/Profile/ProfileView';
 import ChangePassword from './pages/Profile/ChangePassword';
@@ -19,11 +20,16 @@ import Support from './pages/Support/Support';
 import TicketForm from './pages/Support/TicketForm';
 import TicketList from './pages/Support/TicketList';
 import TicketView from './pages/Support/TicketView';
+import MobileLogin from './pages/MobileLogin';
+import LoginMenu from './pages/LoginMenu';
+
 
 
 const routes = (Stack) => (
 	<Stack.Navigator screenOptions={{headerShown: false}}>
+		<Stack.Screen name="LoginMenu" component={LoginMenu} key="loginMenu" />
 		<Stack.Screen name="Login" component={LoginScreen} key="login" />
+		<Stack.Screen name="MobileLogin" component={MobileLogin} />
 		<Stack.Screen name="Support" component={Support} />
 		<Stack.Screen name="TicketForm" component={TicketForm} />
 		<Stack.Screen name="TicketList" component={TicketList} />
@@ -38,6 +44,7 @@ const routes = (Stack) => (
 		<Stack.Screen name="WatchList" component={WatchList} />
 		<Stack.Screen name="Video" component={Video} />
 		<Stack.Screen name="PaymentForm" component={PaymentForm} />
+		<Stack.Screen name="PaymentView" component={PaymentView} />
 		<Stack.Screen name="SearchView" component={SearchView} />
 		<Stack.Screen name="ProfileView" component={ProfileView} />
 		<Stack.Screen name="ChangePassword" component={ChangePassword} />
